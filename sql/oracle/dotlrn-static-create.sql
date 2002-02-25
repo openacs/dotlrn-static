b3560a2c7c617e921f8c4208806fca84cd24b186
Ben Adida <ben@openforce.net>
2002-02-25 15:08:27 +0000

    fixed up

--- sql/oracle/dotlrn-static-create.sql
+++ sql/oracle/dotlrn-static-create.sql
@@ -87,6 +87,15 @@ begin
 	       'TCL'
 	);
 
+	-- RemoveUserFromCommunity
+	foo := acs_sc_impl.new_alias (
+	       'dotlrn_applet',
+	       'dotlrn_static',
+	       'RemoveUserFromCommunity',
+	       'dotlrn_static::remove_user_from_community',
+	       'TCL'
+	);
+
 	-- Add the binding
 	acs_sc_binding.new (
 	    contract_name => 'dotlrn_applet',
