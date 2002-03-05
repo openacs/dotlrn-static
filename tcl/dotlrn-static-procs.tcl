9b64ec46b066b433742c89cf4b6e79da222e99cf
Yonatan Feldman <yon@milliped.com>
2002-03-05 07:19:37 +0000

    merging old and new trees

--- tcl/dotlrn-static-procs.tcl
+++ tcl/dotlrn-static-procs.tcl
@@ -51,8 +51,7 @@ namespace eval dotlrn_static {
 	Add the static applet to dotlrn - for one-time init
 	Must be repeatable!
     } {
-        dotlrn_community::add_applet_to_dotlrn \
-                -applet_key [applet_key]
+        dotlrn_applet::add_applet_to_dotlrn -applet_key [applet_key]
     }
 
     ad_proc -public add_applet_to_community {
