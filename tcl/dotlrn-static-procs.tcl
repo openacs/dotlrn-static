6a6d9c13ce725f9bb980c0df70978715bdb37d86
Yonatan Feldman <yon@milliped.com>
2002-03-29 19:24:12 +0000

    adding license

--- tcl/dotlrn-static-procs.tcl
+++ tcl/dotlrn-static-procs.tcl
@@ -1,4 +1,20 @@
 #
+#  Copyright (C) 2001, 2002 OpenForce, Inc.
+#
+#  This file is part of dotLRN.
+#
+#  dotLRN is free software; you can redistribute it and/or modify it under the
+#  terms of the GNU General Public License as published by the Free Software
+#  Foundation; either version 2 of the License, or (at your option) any later
+#  version.
+#
+#  dotLRN is distributed in the hope that it will be useful, but WITHOUT ANY
+#  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+#  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+#  details.
+#
+
+#
 # Procs for DOTLRN static Applet
 # Copyright 2001 OpenForce, inc.
 # Distributed under the GNU GPL v2
